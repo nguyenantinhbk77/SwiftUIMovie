24da9907f9f848c7af88b370c851a8637c5d6f09
Tinh Nguyen <nguyenantinhbk77@gmail.com>
2024-05-18 18:44:41 +0700

    chore: init project and assest

---
+++ SwiftUIMovie/SwiftUIMovie/App/SwiftUIMovieApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIMovieApp.swift
+//  SwiftUIMovie
+//
+//  Created by Tinh Nguyen on 18/5/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIMovieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
